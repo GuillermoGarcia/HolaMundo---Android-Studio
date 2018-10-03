bbed69da2e4576066335186a518a49993f777ff0
Guillermo García Fernández <guillermogarciaf@gmail.com>
2018-10-03 11:02:10 +0200

    Pulsar Boton

--- app/src/main/java/com/example/usuario/holamundo/MainActivity.java
+++ app/src/main/java/com/example/usuario/holamundo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.usuario.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView msg;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msg = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg.setText(R.string.btn_pulsar);
+            }
+        });
+
     }
 }
